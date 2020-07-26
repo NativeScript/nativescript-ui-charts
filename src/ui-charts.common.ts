c19900a61cec8d4c9ebcc583a97168f85e24f875
Shiva Prasad <sp@shiv19.com>
2020-07-27 10:18:33 +1200

    added support for passing multiline functions for HIFunction

--- src/ui-charts.common.ts
+++ src/ui-charts.common.ts
@@ -1,5 +1,5 @@
-import { View, Property } from '@nativescript/core';
-
+import { View } from '@nativescript/core';
+import { Property } from '@nativescript/core/ui/page';
 export class UIChartsViewBase extends View {
   options: any;
 }
