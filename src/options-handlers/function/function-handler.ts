2842fb900c78a82764b0e3ac4ed581b6ab9d4ab3
Shiva Prasad <sp@shiv19.com>
2020-07-27 12:15:57 +1200

    added support for passing multiline functions for HIFunction

--- src/options-handlers/function/function-handler.ts
+++ src/options-handlers/function/function-handler.ts
@@ -1,9 +1,10 @@
 import { isAndroid } from "@nativescript/core";
 
 export function functionHandler(functionOptions) {
+  const cleanFunction = (fun) => fun.toString().replace(/\n/g, ' ').replace(/\t/g, ' ').replace(/\r/g, ' ');
   const hiFunction = isAndroid ?
-    new com.highsoft.highcharts.core.HIFunction(functionOptions.toString()) :
-    new HIFunction({ JSFunction: functionOptions.toString() });
+    new com.highsoft.highcharts.core.HIFunction(cleanFunction(functionOptions)) :
+    new HIFunction({ JSFunction: cleanFunction(functionOptions) });
 
   return hiFunction;
 }
