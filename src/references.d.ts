c72a01279f81d8559deb12595bec9d56850f6a3c
cjohn <christoph_john@gmx.de>
2021-04-14 17:50:44 +0200

    Updated Angular demo and did some minor cleanups according to https://www.youtube.com/watch?v=u7BQksdfoJo&t=0s

--- src/references.d.ts
+++ src/references.d.ts
@@ -1,5 +1,3 @@
 /// <reference path="./node_modules/@nativescript/types/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-android/index.d.ts" />
-/// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 /// <reference path="./typings/objc!Highcharts.d.ts" />
 /// <reference path="./typings/android-highcharts.d.ts" />
