5a626d38712f5f72d946e75e7f87d4b22f20aafd
cjohn <christoph_john@gmx.de>
2021-07-01 20:34:29 +0200

    fixes bug #26

--- src/ui-charts.android.ts
+++ src/ui-charts.android.ts
@@ -3,110 +3,111 @@ import { optionsHandler } from './options-handlers/options-handler';
 import { Application } from '@nativescript/core';
 import { langHandler } from './options-handlers/lang/lang-handler';
 export class UIChartsView extends UIChartsViewBase {
-    public customLayoutChangeListener;
-    public chartHeight;
-    public chartWidth;
-    public onLoaded() {
-        super.onLoaded();
-        this.customLayoutChangeListener = new android.view.View.OnLayoutChangeListener({
-            onLayoutChange: (v) => {
-                var w = this.nativeView.owner.get();
-                if (w && this.nativeView.getOptions()) {
-                    const newWidth = w.getActualSize().width;
-                    const newHeight = w.getActualSize().height;
-                    if (this.chartHeight !== newHeight) {
-                        if (this.nativeView.getOptions().getChart()) {
-                            this.nativeView.getOptions().getChart().setHeight(new java.lang.Long(newHeight));
-                            this.nativeView.getOptions().getChart().setWidth(new java.lang.Long(newWidth));
-                            this.chartHeight = newHeight;
-                            this.chartWidth = newWidth;
-                            var hiOptions = optionsHandler(this.options);
-                            this.nativeView.setOptions(hiOptions);
-                        }
-                    } 
-                }
+  public customLayoutChangeListener;
+  public chartHeight;
+  public chartWidth;
+  public onLoaded() {
+    super.onLoaded();
+    this.customLayoutChangeListener = new android.view.View.OnLayoutChangeListener({
+      onLayoutChange: (v) => {
+        var w = this.nativeView.owner.get();
+        if (w && this.nativeView.getOptions()) {
+          const newWidth = w.getActualSize().width;
+          const newHeight = w.getActualSize().height;
+          if (this.chartHeight !== newHeight) {
+            if (this.nativeView.getOptions().getChart()) {
+              this.nativeView.getOptions().getChart().setHeight(new java.lang.Long(newHeight));
+              this.nativeView.getOptions().getChart().setWidth(new java.lang.Long(newWidth));
+              this.chartHeight = newHeight;
+              this.chartWidth = newWidth;
+              var hiOptions = optionsHandler(this.options);
+              this.nativeView.setOptions(hiOptions);
             }
-        });
-        this.nativeView.addOnLayoutChangeListener(this.customLayoutChangeListener);
-    }
-
-    public createNativeView() {
-        const chartView = new com.highsoft.highcharts.core.HIChartView(this._context) as any;
-        chartView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
-        return chartView;
-    }
-
-    public onUnloaded() {
-        super.onUnloaded();
-        this.nativeView.removeOnLayoutChangeListener(this.customLayoutChangeListener);
-    }
-
-    /**
-     * Initializes properties/listeners of the native view.
-     */
-    initNativeView(): void {
-        // Attach the owner to nativeView.
-        // When nativeView is tapped we get the owning JS object through this field.
-        (<any>this.nativeView).owner = new WeakRef(this);
-        (<any>this.nativeView).generateDefaultLayoutParams();
-        (<any>this)._orientationHandler = this.onOrientationChange.bind(this);
-        Application.on('orientationChanged', (<any>this)._orientationHandler);
-        super.initNativeView();
-    }
-
-    public disposeNativeView() {
-        Application.off('orientationChanged', (<any>this)._orientationHandler);
-        super.disposeNativeView();
-    }
-
-    onOrientationChange() {
-        setTimeout(() => {
-            const w = (<any>this).nativeView.owner.get();
-            if (w) {
-                // TODO: redraw the chart here to handle orientation change
-            }
-        });
-    }
-
-    public setOptions(opts: any) {
-        this.options = opts;
-        const hiOptions = optionsHandler(this.options);
-        if (this.nativeView) {
-            this.nativeView.setOptions(hiOptions);
+          }
         }
-    }
+      },
+    });
+    this.nativeView.addOnLayoutChangeListener(this.customLayoutChangeListener);
+  }
 
-    public updateOptions(opts) {
-        this.options = opts;
-        const hiOptions = optionsHandler(this.options);
-        if (this.nativeView) {
-            this.nativeView.update(hiOptions);
-        }
-    }
+  public createNativeView() {
+    const chartView = new com.highsoft.highcharts.core.HIChartView(this._context) as any;
+    chartView.setBackgroundColor(android.graphics.Color.TRANSPARENT);
+    return chartView;
+  }
 
-    public setLangOptions(opts: any) {
-        const hiLang = langHandler(opts);
-        if (this.nativeView) {
-            this.nativeView.lang = hiLang;
-        }
+  public onUnloaded() {
+    super.onUnloaded();
+    this.nativeView.removeOnLayoutChangeListener(this.customLayoutChangeListener);
+  }
+
+  /**
+   * Initializes properties/listeners of the native view.
+   */
+  initNativeView(): void {
+    // Attach the owner to nativeView.
+    // When nativeView is tapped we get the owning JS object through this field.
+    (<any>this.nativeView).owner = new WeakRef(this);
+    (<any>this.nativeView).generateDefaultLayoutParams();
+    (<any>this)._orientationHandler = this.onOrientationChange.bind(this);
+    Application.on('orientationChanged', (<any>this)._orientationHandler);
+    super.initNativeView();
+  }
+
+  public disposeNativeView() {
+    Application.off('orientationChanged', (<any>this)._orientationHandler);
+    super.disposeNativeView();
+  }
+
+  onOrientationChange() {
+    setTimeout(() => {
+      const w = (<any>this).nativeView.owner.get();
+      if (w) {
+        // TODO: redraw the chart here to handle orientation change
       }
+    });
+  }
 
-    public setExtremes(newMin: any, newMax: any, xAxisIndex = 0) {
-        const nativeview = (<any>this.nativeView);
-        const opts = (nativeview.getOptions() as com.highsoft.highcharts.common.hichartsclasses.HIOptions);
-        const xaxisArr = opts.getXAxis();
-        const xaxis = xaxisArr.get(xAxisIndex);
-        xaxis.setMin(new java.lang.Long(newMin));
-        xaxis.setMax(new java.lang.Long(newMax));
-        nativeview.zoomOut();
-        nativeview.update(opts);
+  public setOptions(opts: any) {
+    this.options = opts;
+    const hiOptions = optionsHandler(this.options);
+    if (this.nativeView) {
+      this.nativeView.setOptions(hiOptions);
+      this.nativeView.reload();
     }
+  }
 
-    public enableAnnotationsModule() {
-        if (this.nativeView) {
-            const nativeArray = new java.util.ArrayList<any>();
-            nativeArray.add("annotations");
-            this.nativeView.plugins = nativeArray;
-        }
-    };
+  public updateOptions(opts) {
+    this.options = opts;
+    const hiOptions = optionsHandler(this.options);
+    if (this.nativeView) {
+      this.nativeView.update(hiOptions);
+    }
+  }
+
+  public setLangOptions(opts: any) {
+    const hiLang = langHandler(opts);
+    if (this.nativeView) {
+      this.nativeView.lang = hiLang;
+    }
+  }
+
+  public setExtremes(newMin: any, newMax: any, xAxisIndex = 0) {
+    const nativeview = <any>this.nativeView;
+    const opts = nativeview.getOptions() as com.highsoft.highcharts.common.hichartsclasses.HIOptions;
+    const xaxisArr = opts.getXAxis();
+    const xaxis = xaxisArr.get(xAxisIndex);
+    xaxis.setMin(new java.lang.Long(newMin));
+    xaxis.setMax(new java.lang.Long(newMax));
+    nativeview.zoomOut();
+    nativeview.update(opts);
+  }
+
+  public enableAnnotationsModule() {
+    if (this.nativeView) {
+      const nativeArray = new java.util.ArrayList<any>();
+      nativeArray.add('annotations');
+      this.nativeView.plugins = nativeArray;
+    }
+  }
 }
