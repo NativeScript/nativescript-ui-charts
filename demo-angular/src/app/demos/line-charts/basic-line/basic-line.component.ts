b810bf060eabfd29a5190a8d847d708932b47cd6
cjohn <christoph_john@gmx.de>
2021-08-13 00:51:00 +0200

    fixes issue 28, updates ios highcharts to latest 8

--- demo-angular/src/app/demos/line-charts/basic-line/basic-line.component.ts
+++ demo-angular/src/app/demos/line-charts/basic-line/basic-line.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { EventData } from '@nativescript/core';
 import { UIChartsView } from '@nativescript/ui-charts';
 
@@ -8,9 +8,19 @@ import { UIChartsView } from '@nativescript/ui-charts';
 })
 export class BasicLineComponent implements OnInit {
   private _chart: UIChartsView;
+  public _langOptions = {};
+  public _updateChartContent = true;
+
   // onAndroid it is required for you to pass a series skeleton if the data
   // will be loaded asynchronously
-  chartOptions: any = {
+  chartOptions: any;
+  chartOptionsBase: any = {
+    title: {
+      text: 'Empty Chart',
+    },
+    subtitle: {
+      text: 'A subtitle',
+    },
     legend: {
       enabled: false,
     },
@@ -50,6 +60,7 @@ export class BasicLineComponent implements OnInit {
   };
   //////////////////////////////////////////////////////////////////////////////////////////////////
   ngOnInit(): void {
+    this.chartOptions = JSON.parse(JSON.stringify(this.chartOptionsBase));
     this.chartOptions = {
       title: {
         text: 'Solar Employment Growth by Sector, 2010-2016',
@@ -123,63 +134,63 @@ export class BasicLineComponent implements OnInit {
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  onDataChange(args: EventData) {
-    if (this._chart) {
-      if (this.chartOptions.title.text === 'Title Changed') {
-        this.chartOptions.title.text =
-          'Title Changed Again into a very long sentence making it span across multiple lines';
-        this.chartOptions.subtitle.text = 'Subtitle Changed Again';
-        this.chartOptions.series = [
-          {
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
-          },
-          {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-          },
-          {
-            name: 'Sales & Distribution',
-            data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
-          },
-          {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
-          },
-          {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-          },
-        ];
-      } else {
-        this.chartOptions.title.text = 'Title Changed';
-        this.chartOptions.subtitle.text = 'Subtitle Changed';
-        this.chartOptions.series = [
-          {
-            name: 'Project Development',
-            data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227].reverse(),
-          },
-          {
-            name: 'Installation',
-            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175].reverse(),
-          },
-          {
-            name: 'Manufacturing',
-            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
-          },
-          {
-            name: 'Other',
-            data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
-          },
-        ];
-      }
-      this._chart.updateOptions(this.chartOptions);
-    }
+  onReloadChart(args: EventData) {
+    this._updateChartContent = false;
+    this.chartOptions = JSON.parse(JSON.stringify(this.chartOptionsBase));
   }
-
   //////////////////////////////////////////////////////////////////////////////////////////////////
-  public chartLoaded(args: EventData) {
-    this._chart = args.object as UIChartsView;
-    this._chart.setOptions(this.chartOptions);
+  onDataChange(args: EventData) {
+    this._updateChartContent = true;
+    // create shallow copy in order for change detection to run
+    this.chartOptions = Object.assign({}, this.chartOptions);
+
+    if (this.chartOptions.title.text === 'Title Changed') {
+      this.chartOptions.title.text =
+        'Title Changed Again into a very long sentence making it span across multiple lines';
+      this.chartOptions.subtitle.text = 'Subtitle Changed Again';
+      this.chartOptions.series = [
+        {
+          name: 'Installation',
+          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175],
+        },
+        {
+          name: 'Manufacturing',
+          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+        },
+        {
+          name: 'Sales & Distribution',
+          data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387],
+        },
+        {
+          name: 'Project Development',
+          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227],
+        },
+        {
+          name: 'Other',
+          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+        },
+      ];
+    } else {
+      this.chartOptions.title.text = 'Title Changed';
+      this.chartOptions.subtitle.text = 'Subtitle Changed';
+      this.chartOptions.series = [
+        {
+          name: 'Project Development',
+          data: [null, null, 7988, 12169, 15112, 22452, 34400, 34227].reverse(),
+        },
+        {
+          name: 'Installation',
+          data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175].reverse(),
+        },
+        {
+          name: 'Manufacturing',
+          data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434],
+        },
+        {
+          name: 'Other',
+          data: [12908, 5948, 8105, 11248, 8989, 11816, 18274, 18111],
+        },
+      ];
+    }
   }
 }
